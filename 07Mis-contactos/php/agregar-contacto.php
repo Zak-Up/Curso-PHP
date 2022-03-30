f33a78662d81a427f251d798d400a4106e4988da
ZakyUp <andresjr195@hotmail.com>
2022-03-29 20:58:08 -0500

    crud

---
+++ 07Mis-contactos/php/agregar-contacto.php
@@ -0,0 +1,47 @@
+<?php
+//Asigno a variabls de php los valores que vienen del formuario en el atributo name
+$email = $_POST["email_txt"];
+$nombre = $_POST["nombre_txt"];
+$sexo = $_POST["sexo_rdo"];
+$nacimiento = $_POST["nacimiento_txt"];
+$telefono = $_POST["telefono_txt"];
+$pais = $_POST["pais_slc"];
+
+//Dependiendo el sexo colocamos una imagen generica
+$imagen_generica = ($sexo=="M")?"img/fotos/amigo.png":"img/fotos/amiga.png";
+
+//Verificamos que no exista previamente el email del usuario de la BD
+include("conexion.php");
+$consulta = "SELECT * FROM contactos WHERE email='$email'";
+$ejecutar_consulta = $conexion->query($consulta);
+$num_regs = $ejecutar_consulta->num_rows;
+
+//Si $num_regs es igual a 0, insertamos datos en la tabla, sino mandamos un mensaje que diga el usuario ya existe
+if($num_regs == 0){
+    
+    //Se ejecuta la funcion para subir la imagen
+    include("funciones.php");
+    $tipo = $_FILES["foto_fls"]["type"];
+    $archivo = $_FILES["foto_fls"]["tmp_name"];
+    $se_subio_imagen = subir_imagen($tipo,$archivo,$email);
+
+    //Si la foto en el formulario viene vacia, entonces le asigno el valor de la imagen generica, sino entonces el nombre de la foto que se subio
+    $imagen = empty($archivo)?$imagen_generica:$se_subio_imagen;
+
+    $consulta = "INSERT INTO contactos (email,nombre,sexo,nacimiento,telefono,pais,imagen) VALUES ('$email','$nombre','$sexo','$nacimiento','$telefono','$pais','$imagen')";
+
+    $ejecutar_consulta = $conexion->query(($consulta));
+
+    if($ejecutar_consulta)
+        $mensaje = "Se ha dado de alta al contacto con el email <b>$email</b>";
+
+    else
+        $mensaje = "No se pudo dar de alta al contacto con el email <b>$email</b>";
+} 
+else {
+    $mensaje = "No se pudo dar de alta al contacto con el email <b>$email</b> porque ya existe";
+}
+
+$conexion->close();
+header("Location: ../index.php?op=alta&mensaje=$mensaje");
+?>
--- 07Mis-contactos/php/operaciones-basicas.php
+++ 07Mis-contactos/php/operaciones-basicas.php
@@ -9,31 +9,33 @@ echo "<h1>Las cuatro operaciones basicas a una BD</h1>";
 echo "<h2>1)INSERCION DE DATOS</h2>";
 
 //INSERT INTO nombre_tabla (campos_tabla) VALUES (valores_campos)
-$consulta = "INSERT INTO contactos (email,nombre,sexo,nacimiento,telefono,pais,imagen) VALUES('andresjr195@hotmail.com','Andres Restrepo','M','1992-04-12','573133990387','Colombia','and.png')";
+$consulta = "INSERT INTO contactos (email,nombre,sexo,nacimiento,telefono,pais,imagen) VALUES('transplants195@hotmail.com','Andres Restrepo','M','1992-04-12','573133990387','Colombia','and.png')";
 
 $ejecutar_consulta = mysqli_query($conexion,$consulta);
 echo "Se han insertado los datos <br>";
 
-echo "<h2>ELIMINACION DE DATOS</h2>";
+echo "<h2>2)ELIMINACION DE DATOS</h2>";
 
 // DELETE FROM nombre_tabla WHERE campo = valor
-$consulta = "DELETE FROM contactos WHERE email = 'andresjr195@hotmail.com'";
+$consulta = "DELETE FROM contactos WHERE email = 'transplants@hotmail.com'";
 
-// $ejecutar_consulta = mysqli_query($conexion,$consulta);
+$ejecutar_consulta = mysqli_query($conexion,$consulta);
 echo "Datos eliminados <br>";
 
-echo "<h2>MODIFICAR DATOS</h2>";
+echo "<h2>3)MODIFICAR DATOS</h2>";
 //UPDATE nombre_tabla SET nombre_campo = valor_campo, otro_campo = otro_valor WHERE campo = valor
-$consulta = "UPDATE contactos SET email = 'transplants@hotmail.com', nombre = 'Zak Up', imagen = 'andres.png' WHERE email = 'andresjr195@hotmail.com'";
+$consulta = "UPDATE contactos SET email = 'transplants@hotmail.com', nombre = 'Zak Up', imagen = 'zakup.png' WHERE email = 'transplants195@hotmail.com'";
 
 $ejecutar_consulta = mysqli_query($conexion,$consulta);
-echo "Se han actualizado los datos <br>";
+echo "Se han modificado los datos <br>";
 
-echo "<h2>CONSULTA DE DATOS</h2>";
+echo "<h2>4)CONSULTA DE DATOS</h2>";
 //SELECT * FROM nombre_tabla WHERE campo = valor
-$consulta = "SELECT * FROM contactos WHERE email = 'transplants@hotmail.com'";
+$consulta = "SELECT * FROM contactos";
 
 $ejecutar_consulta = mysqli_query($conexion,$consulta);
+
+echo "<h3>Consulta que trae todos los registros de la tabla</h3>";
 
 while($registro=mysqli_fetch_array($ejecutar_consulta)){
     echo $registro["email"]."---";
@@ -43,6 +45,41 @@ while($registro=mysqli_fetch_array($ejecutar_consulta)){
     echo $registro["telefono"]."---";
     echo $registro["pais"]."---";
     echo $registro["imagen"]."---";
+    echo "<br>";
+};
+
+$consulta = "SELECT * FROM contactos WHERE email = 'transplants@hotmail.com'";
+
+$ejecutar_consulta = mysqli_query($conexion,$consulta);
+
+echo "<h3>Consulta que trae solo los registros de la tabla seleccionada</h3>";
+
+while($registro=mysqli_fetch_array($ejecutar_consulta)){
+    echo $registro["email"]."---";
+    echo $registro["nombre"]."---";
+    echo $registro["sexo"]."---";
+    echo $registro["nacimiento"]."---";
+    echo $registro["telefono"]."---";
+    echo $registro["pais"]."---";
+    echo $registro["imagen"]."---";
+    echo "<br>";
+}
+
+$consulta = "SELECT * FROM contactos WHERE nombre = 'Andres Restrepo' AND imagen = 'and.png'" ;
+
+$ejecutar_consulta = mysqli_query($conexion,$consulta);
+
+echo "<h3>Consulta que trae solo los registros de la tabla con informacion compartida</h3>";
+
+while($registro=mysqli_fetch_array($ejecutar_consulta)){
+    echo $registro["email"]."---";
+    echo $registro["nombre"]."---";
+    echo $registro["sexo"]."---";
+    echo $registro["nacimiento"]."---";
+    echo $registro["telefono"]."---";
+    echo $registro["pais"]."---";
+    echo $registro["imagen"]."---";
+    echo "<br>";
 }
 
 ?>
